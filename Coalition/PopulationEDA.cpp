7ee1254ae6b1ed127e2a93bff96070106bff6b07
caitaozhan <caitaozhan@163.com>
2016-09-07 12:29:48 +0800

    first step for EDA

---
+++ Coalition/PopulationEDA.cpp
@@ -0,0 +1,6 @@
+#include "PopulationEDA.h"
+
+PopulationEDA::PopulationEDA()
+{
+
+}
---
+++ Coalition/PopulationEDA.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "Coalition.h"
+#include "../Analyze/AnalyzeLog.h"
+using namespace std;
+
+class PopulationEDA
+{
+public:
+	PopulationEDA();
+
+private:
+
+};
