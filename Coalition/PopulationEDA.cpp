34b571b4c5719cbb3c88fff129a875fe47d4f09a
caitaozhan <caitaozhan@163.com>
2016-09-12 18:17:15 +0800

    a printing issue, then is ready to run test cases

--- Coalition/PopulationEDA.cpp
+++ Coalition/PopulationEDA.cpp
@@ -1,12 +1,12 @@
 #include "PopulationEDA.h"
 
-string PopulationEDA::LOG_EXPER_EVALUATE = string("../log/EDA/case-8/experiment_");         // 程序运行日志，记录每一次实验的评估值
-string PopulationEDA::LOG_ANALYSE_OUTPUT = string("../log/EDA/case-8/result_");             // 分析程序运行的运行记录
+string PopulationEDA::LOG_EXPER_EVALUATE = string("../log/EDA/case-4/experiment_");         // 程序运行日志，记录每一次实验的评估值
+string PopulationEDA::LOG_ANALYSE_OUTPUT = string("../log/EDA/case-4/result_");             // 分析程序运行的运行记录
 
 PopulationEDA::PopulationEDA()
 {
 	ENEMY_INPUT = string("../sample/4_case_20.txt");                           // enemy阵型的初始化编队
-	LOG_PM_NAME = string("../log/EDA/case-8/log_simpleEvaluate.txt");  // 概率矩阵日志
+	LOG_PM_NAME = string("../log/EDA/case-4/log_simpleEvaluate.txt");  // 概率矩阵日志
 	MAX_UPDATE = 10000;
 	MAX_EXPERIMENT = 15;
 
